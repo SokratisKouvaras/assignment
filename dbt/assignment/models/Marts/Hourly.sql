92333b426f3f6765f4563eacf70ab2b4e6092d6b
Sokratis <sokrateskouv@gmail.com>
2024-04-23 15:14:25 +0200

    add hourly model

--- dbt/assignment/models/Marts/Hourly.sql
+++ dbt/assignment/models/Marts/Hourly.sql
@@ -1,11 +1,12 @@
 {{ config(alias='Hourly') }}
 
 SELECT
-    request_date,
-    count(dw_trip_id) as number_of_trips
+    pickup_hour,
+    avg(trip_miles) as average_travel_distance_in_miles,
+    avg(trip_time) as average_travel_duration_in_seconds
 FROM 
     {{ref("DCZone_DC_Trips")}}
 GROUP BY
-    request_date
-ORDER BY request_date ASC
+    pickup_hour
+ORDER BY pickup_hour ASC
 
